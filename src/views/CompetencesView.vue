5ae5b4239439e0a0f04902aad8b974844816a9b4
mkuciapki <m.kuciapski@gmail.com>
2026-04-11 12:30:27 +0200

    styles

--- src/views/CompetencesView.vue
+++ src/views/CompetencesView.vue
@@ -1,18 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { competences } from '@/assets/competences'
+
+// #region LIST
+const _competences = ref(competences)
+
+//#endregion LIST
+
+// #region SEARCH
+const _searchText = ref('')
+//#endregion SEARCH
+</script>
 
 <template>
-  <main>dwie rzeczy są nieskończone - wszechświat i ludzka głupota</main>
-
-  <div class="card bg-base-100 w-96 shadow-sm">
-    <figure>
-      <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
-    </figure>
-    <div class="card-body">
-      <h2 class="card-title">Card Title</h2>
-      <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
-      </div>
+  <div class="flex justify-center w-full!">
+    <div class="join w-[50%]!">
+      <button class="btn join-item text-white bg-green-800">Podaj nazwę kompetencji</button>
+      <input type="text" class="w-full!" v-model="_searchText" />
     </div>
+  </div>
+  <div class="flex flex-wrap justify-center gap-3">
+    <template v-for="competence in _competences" :key="competence.Id">
+      <div v-if="competence.Level" class="card bg-base-100 w-48 shadow-sm">
+        <h2 class="text-center text-xl uppercase">{{ competence.Name }}</h2>
+        <img :src="`assets/${competence.Picture}`" />
+        <div class="card-body">
+          <!--<p :style="{color: competence.Level > 3 ? 'green' : (competence.Level <3 ? 'red' : 'black')}">Poziom: {{competence.Level}}</p> -->
+          <p :class="[competence.Level < 3 ? 'text-red-500' : competence.Level > 3 ? 'text-green-500' : '']">Poziom: {{ competence.Level }}</p>
+          <p v-if="competence.Description">Opis: {{ competence.Description }}</p>
+        </div>
+      </div>
+    </template>
   </div>
 </template>
--- src/views/TempView.vue
+++ src/views/TempView.vue
@@ -3,20 +3,23 @@ import { ref } from 'vue'
 import { users } from '@/assets/users'
 
 const _users = ref(users)
-
 const _user = ref({ Name: '<b>John Doe</b>', Age: 30 })
 
 setTimeout(() => {
   _user.value = { Name: '<b>Michał K</b>', Age: 30 }
-
   _users.value = users.filter((u) => u.Age > 40)
+}, 5000)
+
+const _fontSize = ref('12px')
+setTimeout(() => {
+  _fontSize.value = new Date().getSeconds() + 'px'
 }, 5000)
 </script>
 
 <template>
   <div>
     <h1>TEMP</h1>
-    <p>{{ _user.Name }}</p>
+    <p class="user-name">{{ _user.Name }}</p>
     <p v-html="_user.Name"></p>
 
     <input v-model.lazy.trim="_user.Name" />
@@ -29,3 +32,8 @@ setTimeout(() => {
     </div>
   </div>
 </template>
+<style scoped>
+.user-name {
+  font-size: v-bind('_fontSize');
+}
+</style>
