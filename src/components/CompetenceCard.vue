163dad58a93a7f29d742e05feccd058c274bbeee
mkuciapki <m.kuciapski@gmail.com>
2026-04-12 15:00:50 +0200

    ComptenceCard props basic

---
+++ src/components/CompetenceCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const _props = defineProps(['competence'])
+</script>
+<template>
+  <div v-if="competence.Level" class="card bg-base-100 w-48 shadow-sm">
+    <h2 class="text-center text-xl uppercase">{{ competence.Name }}</h2>
+    <img :src="`assets/${competence.Picture}`" />
+    <div class="card-body">
+      <p :class="[competence.Level < 3 ? 'text-red-500' : competence.Level > 3 ? 'text-green-500' : '']">Poziom: {{ competence.Level }}</p>
+      <p v-if="competence.Description">Opis: {{ competence.Description }}</p>
+    </div>
+  </div>
+</template>
+<style scoped></style>
--- src/views/CompetencesView.vue
+++ src/views/CompetencesView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed, ref, watch } from 'vue'
 import { competences } from '@/assets/competences'
+import CompetenceCard from '@/components/CompetenceCard.vue'
 
 // #region LIST
 const _competences = ref(competences)
@@ -41,17 +42,7 @@ watch(
     </div>
   </div>
   <div class="flex flex-wrap justify-center card-spacing">
-    <template v-for="competence in _filteredCompetences" :key="competence.Id">
-      <div v-if="competence.Level" class="card bg-base-100 w-48 shadow-sm">
-        <h2 class="text-center text-xl uppercase">{{ competence.Name }}</h2>
-        <img :src="`assets/${competence.Picture}`" />
-        <div class="card-body">
-          <!--<p :style="{color: competence.Level > 3 ? 'green' : (competence.Level <3 ? 'red' : 'black')}">Poziom: {{competence.Level}}</p> -->
-          <p :class="[competence.Level < 3 ? 'text-red-500' : competence.Level > 3 ? 'text-green-500' : '']">Poziom: {{ competence.Level }}</p>
-          <p v-if="competence.Description">Opis: {{ competence.Description }}</p>
-        </div>
-      </div>
-    </template>
+    <CompetenceCard v-for="competence in _filteredCompetences" :key="competence.Id" :competence="competence" />
   </div>
   <div v-show="_alert" class="toast">
     <div class="alert alert-info">
